d1bf367963b02350df8a4a36f6afd82be986cd67
0xtrou <khangtran.btc@gmail.com>
2024-04-14 16:02:21 +0700

    chore: enabled runes indexing

--- ord.sh
+++ ord.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-$(pwd)/ord/target/release/ord --regtest --bitcoin-rpc-username soloengineer --bitcoin-rpc-password soloengineer --data-dir ./ord-data "$@"
+$(pwd)/ord/target/release/ord --index-runes --regtest --bitcoin-rpc-username soloengineer --bitcoin-rpc-password soloengineer --data-dir ./ord-data "$@"
